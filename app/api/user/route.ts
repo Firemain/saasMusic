03cc66b9314f89146e1dc883c6b2aedc036216f1
Firemain <87fugiii@gmail.com>
2024-11-24 21:34:10 +0800

    update route delete user

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,22 +1,25 @@
 import { auth } from "@/auth";
-
+import { NextRequest } from "next/server";
 import { prisma } from "@/lib/db";
 
-export const DELETE = auth(async (req) => {
-  if (!req.auth || !req.auth.user) {
-    // If no authentication or user is found, immediately return a 401 error
+export async function DELETE(
+  req: NextRequest,
+  context: { params: Record<string, string | string[]> }
+) {
+  const session = await auth();
+  
+  if (!session || !session.user) {
     return new Response("Not authenticated or Invalid user", { status: 401 });
   }
 
   try {
     await prisma.user.delete({
       where: {
-        id: req.auth.user.id,
+        id: session.user.id,
       },
     });
     return new Response("User deleted successfully!", { status: 200 });
   } catch (error) {
-    // Return a 500 error if something goes wrong in the deletion process
     return new Response("Internal server error", { status: 500 });
   }
-});
+}
